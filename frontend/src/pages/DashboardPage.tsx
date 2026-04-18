c7ba691a273b6e166d9f35574ebd15efa1090a18
Iuliia Usatova <millenium4363@gmail.com>
2026-04-18 01:45:52 -0700

    feat: add DashboardPage with logs statistics

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,6 +1,18 @@
+import { useLogsStore } from '../stores/logsStore'
+
 function DashboardPage() {
+    const { logs } = useLogsStore()
+    const preventedLeaks = logs.filter((log) => log.originalText !== log.maskedText).length
+    const totalCost = logs.reduce((acc, log) => {
+        return acc + log.cost
+    }, 0)
     return (
-        <h1>Дашборд</h1>
+        <div>
+            <h1>Дашборд</h1>
+            <p>Всего запросов: {logs.length}</p>
+            <p>Предотвращённых утечек: {preventedLeaks}</p>
+            <p>Общая стоимость : {totalCost}</p>
+        </div>
     )
 }
 
